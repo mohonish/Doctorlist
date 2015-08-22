2edf74a0ed04f54bf9feb0379c2b35ed476b17b1
Mohonish Chakraborty <mohonish.c@gmail.com>
2015-08-22 20:29:34 +0530

    initial commit

---
+++ DoctorList/DoctorListTableViewCell.swift
@@ -0,0 +1,24 @@
+//
+//  DoctorListTableViewCell.swift
+//  DoctorList
+//
+//  Created by Mohnish Chakaraborty on 22/08/15.
+//  Copyright © 2015 mohonish. All rights reserved.
+//
+
+import UIKit
+
+class DoctorListTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
---
+++ README.md
@@ -0,0 +1,1 @@
+Topcoder iOS Swift Challenge - Hospital Doctors List
